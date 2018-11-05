9d8527775099e3b3527e9a5b57f195e6c09bca85
Naga Dorga Mani <d.naga.dorga.mani@accenture.com>
2018-11-05 17:19:38 +0530

    springboot sample Project

---
+++ spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplication.java
@@ -0,0 +1,10 @@
+package com.in28minutes.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootMicroserviceEurekaNamingServerApplication {
+
+}
